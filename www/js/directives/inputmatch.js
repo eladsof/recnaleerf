ce05821384da55731365c78c6067dcd87cd665ab
Elad Sofer <elad.sofer@gmail.com>
2014-08-19 15:07:38 +0300

    fixed animation and validation and changed to angular 1.3

--- www/js/directives/inputmatch.js
+++ www/js/directives/inputmatch.js
@@ -9,7 +9,10 @@ angular.module('inputMatch',[])
             link: function(scope, elem, attrs, ctrl) {
 
                 scope.$watch(function() {
-                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+                    console.log(ctrl);
+
+                    console.log(scope.match+' === '+ ctrl.$viewValue);
+                    return (ctrl.$pristine && angular.isUndefined(ctrl.$viewValue)) || scope.match === ctrl.$viewValue;
                 }, function(currentValue) {
                     ctrl.$setValidity('match', currentValue);
                 });
